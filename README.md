15d3383dab801d85a31bbef879485d7521f4d5e1
tetsmaster <65034001+tetsmaster@users.noreply.github.com>
2020-05-11 20:32:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Monopoly bot
+this is not a bot for monopoly game
+### how does it work
+it types random message about X owning Y. e.g.: npm is now a part of Github
+it reacts to /monopoly, monopoly and монополия in the chat
+### how to launch it
+1. change $botkey value to your bot token in monopoly.ps1(you can get it from BotFather)
+2. launch monopoly.ps1 with powershell(obviously lol)
+3. it werks
---
+++ monopoly.ps1
@@ -0,0 +1,33 @@
+﻿$botkey = "" #insert bot token. DO NOT SHARE THIS FILE WITHOUT REMOVING IT
+
+$source = 'node.js', 'npm', 'wget', 'Source SDK', 'VKontakte', 'fasm', 'PHP', 'MySQL', 'MongoDb', 'git' # X is a part of Y
+$dest = 'Github', 'mail.ru', 'Gitlab', 'Microsoft'                                                     # where X is $source and Y is $dest
+# P.S.: i didn't bother to add code to add everything from file. Too bad!
+
+
+$offset = -1
+function SendMsg($text) {
+  #this shit is here because i didn't bother to put it in one line and because is i did it would be a mess of a code. Too bad!
+  $body = @{
+    chat_id = $json.result[$i].message.chat.id
+    text    = "$text"
+  }
+                
+  Write-Host "message: "+$body.text
+  Invoke-WebRequest -Uri "https://api.telegram.org/bot$botkey/sendMessage" -Body $body 
+}
+while ($true) {
+  $json = Invoke-WebRequest -Uri $getUpdatesLink -Body @{ offset = $offset } | ConvertFrom-Json # this works with a primitive method. it checks new messages every second(you can change it in the config)
+  $l = $json.result.length
+  $i = 0
+
+  while ($i -lt $l) {
+    $offset = $json.result[$i].update_id + 1
+    if ($json.result[$i].message.text -match "(.*)monopol(.*)" -OR $json.result[$i].message.text -match "(.*)монопол(.*)") {
+      #it supports english, russian and plenty of other languages
+      SendMsg($source[(Get-Random -Maximum ([array]$source).count)] + " is now a part of " + $dest[(Get-Random -Maximum ([array]$dest).count)])
+    }
+    $i++
+    Start-Sleep -s 1
+  }
+}
